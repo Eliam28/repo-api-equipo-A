bf8f8c6737235bd20fc98f332d202caeef9096d2
RobertoYB <ryupit26@gmail.com>
2026-02-15 17:01:38 -0600

    Corrección de rama
    Cree otra rama sin hacer anda en la terminal para tenerla aqui y no reiniciar de nuevo la rama

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.providers_o.routes import router as providers_router
+from app.API.providers_o.routes import router as providers_router
+from app.API.stock.routes import router as stock_router
 
 router = APIRouter()
 
@@ -7,4 +8,10 @@ router.include_router(
     providers_router,
     prefix="/providers",
     tags=["Providers"]
+)
+
+router.include_router(
+    stock_router,
+    prefix="/stock",
+    tags=["Stock"]
 )
