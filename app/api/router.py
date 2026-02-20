42808a8dfd49aff714a5401cb98b636fe3ee1ae4
RobertoYB <ryupit26@gmail.com>
2026-02-19 22:05:17 -0600

    Integracion de endpoint en router.py
    Ahora si sale en la documentacion de fastapi mi endpoint funcional

--- app/api/router.py
+++ app/api/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.API.providers_o.routes import router as providers_router
 from app.API.products.routes import router as products_router
 from app.API.orders.routes import router as orders_router
+from app.API.stock.routes import router as stock_router  
 
 router = APIRouter()
 
@@ -10,13 +11,21 @@ router.include_router(
     prefix="/providers",
     tags=["Providers"]
 )
+
 router.include_router(
     products_router,
     prefix="/products",
     tags=["Products"]
 )
+
 router.include_router(
     orders_router,
     prefix="/orders",
     tags=["Orders"]
+)
+
+router.include_router(  
+    stock_router,
+    prefix="/stock",
+    tags=["Stock"]
 )
