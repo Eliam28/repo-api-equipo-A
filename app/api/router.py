b5d40b37baa76c64f13c4db0a7704bbe1d58dbb3
RobertoYB <ryupit26@gmail.com>
2026-02-19 21:30:51 -0600

    Resolucion de conflictos al hacer merge
    Hice lo que github me recomendo para hacer merge a dev

--- app/api/router.py
+++ app/api/router.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from app.API.providers_o.routes import router as providers_router
-from app.API.stock.routes import router as stock_router
 from app.API.products.routes import router as products_router
 from app.API.orders.routes import router as orders_router
 
@@ -11,19 +10,11 @@ router.include_router(
     prefix="/providers",
     tags=["Providers"]
 )
-
-router.include_router(
-    stock_router,
-    prefix="/stock",
-    tags=["Stock"]
-)
-
 router.include_router(
     products_router,
     prefix="/products",
     tags=["Products"]
 )
-
 router.include_router(
     orders_router,
     prefix="/orders",
