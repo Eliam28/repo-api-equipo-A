ef771d882eae8fd396f1d4343480710175622301
KevinGarabita <152626186+KevinGarabita@users.noreply.github.com>
2026-02-24 19:23:24 -0600

    endpoint proveedores

--- app/API/router_prestashop.py
+++ app/API/router_prestashop.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.API.prestashop.products.routes import router as products_router
 from app.API.prestashop.orders.routes import router as orders_router
 from app.API.prestashop.payments.routes import router as payments_router
+from app.API.prestashop.proveedores.routes import router as proveedores_router
 
 router = APIRouter()
 
@@ -21,4 +22,10 @@ router.include_router(
     payments_router,
     prefix="/payments",
     tags=["Payments"]
+)
+
+router.include_router(
+    proveedores_router,
+    prefix="/suppliers",
+    tags=["Suppliers"]
 )
