991c2f8fede5f46fd92ad39da3c7cd2c2d6d5928
Scorpion-CC <carloscantom@gmail.com>
2026-02-28 05:45:47 -0600

    Endpoint delete terminado
    Se terminó de implementar el endpoint para desactivar productos de prestashop.

--- app/API/bridge/products/routes.py
+++ app/API/bridge/products/routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.API.bridge.products.service import sync_products, create_one_product_by_reference
 from app.API.prestashop.utils import ps_success, ps_error
 from app.Core.prestashop_client import prestashop_get, prestashop_put
-from app.API.bridge.products.mapper_product_update import product_update
+from app.API.bridge.products.mapper_product_deactivation import product_update
 
 
 router = APIRouter()
@@ -36,28 +36,25 @@ def deactivate_product(reference: str):
         
         product = data['products'][0]
         id = product['id']
+        price = product['price']
 
         if (isinstance(product['name'], list)):
            name = product['name'][0]['value']
         else:
            name = product['name']
-        
-        if (isinstance(product['link_rewrite'], list)):
-           slug = product['link_rewrite'][0]['value']
-        else:
-           slug = product['link_rewrite']
 
-        xml = product_update(
-          id = id,
-          active = "0",
-          name = name,
-          slug = slug,
-          reference = reference
-        )
+        xml = product_update(id = id, active = "0", price = price)
 
         prestashop_put(f"products/{id}", xml)
 
-        return ps_success(data)
+        updated_product = {
+           "id": id,
+            "name": name,
+            "reference": reference,
+            "active": "0",
+        }
+
+        return ps_success(updated_product)
     
     except Exception as e:
         return ps_error("500",str(e) )
